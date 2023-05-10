25adaf323b59f17c98734d7787b54ac587a92920
freedox10 <alfre2636@yahoo.com.ar>
2023-05-10 20:52:25 -0300

    Carga del zip Initializr en Netbeans y creacion de BD backendaaf en MySQL

---
+++ src/main/java/com/portfolio/aaf/AafApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.aaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/aaf/AafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.aaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
