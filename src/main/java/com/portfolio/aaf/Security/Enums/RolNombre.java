fbc6d0bd81d6e643e2e6d7ff540f744f10732e4f
freedox10 <alfre2636@yahoo.com.ar>
2023-05-11 13:19:22 -0300

    Creando Login y JWT

---
+++ src/main/java/com/portfolio/aaf/Security/Enums/RolNombre.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.aaf.Security.Enums;
+
+/**
+ *
+ * @author DNVN-DDPS
+ */
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
